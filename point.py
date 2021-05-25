2df1b8048e57ecbe011abecea5624b481c320bd1
kimjjing1004 <kimjjing1004@naver.com>
2021-05-25 16:50:24 +0900

    Refactoring : 연산자 오버로딩

--- point.py
+++ point.py
@@ -15,3 +15,56 @@ class Point:
 
     def getY(self):
         return self.y
+
+    # 생성자 : __init__
+    def __init__(self, x = 0, y = 0):   # x, y를 생성자 매개변수
+        self.x, self.y = x, y
+        Point.instance_count += 1
+
+    # 소멸자 : __del__
+    def __del__(self):
+        Point.instance_count -= 1
+
+    # 문자열 출력 포맷
+    def __str__(self):
+        return "Point x={}, y={}".format(self.x, self.y)
+
+    # 문자열 출력 포맷
+    def __repr__(self):
+        return "Point({}, {})".format(self.x, self.y)
+
+    # 연산자 오버로딩 +
+    def __add__(self, other):
+        # Point + other
+        if isinstance(other, Point):    # + Point
+            return Point(self.x + other.x,
+                         self.y + other.y)
+        elif isinstance(other, int):    # + int
+            return Point(self.x + other,
+                         self.y + other)
+
+        return self + other
+
+    # 역이행 연산자 +
+    def __radd__(self, other):  # other + Point
+        if isinstance(other, int):
+            return Point(self.x + other,
+                         self.y + other)
+
+        return other + self
+
+    # - 연산자 오버로딩
+    def __sub__(self, other):
+        if isinstance(other, Point):
+            return Point(self.x - other.x,
+                         self.y - other.y)
+        elif isinstance(other, int):
+            return Point(self.x - other,
+                         self.y - other)
+
+        return self - other
+
+
+    # == 오버로딩
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
