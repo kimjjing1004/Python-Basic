3c7f49aaef2480f264595f85c8fe9bc1d8cabe6a
kimjjing1004 <kimjjing1004@naver.com>
2021-07-12 00:12:41 +0900

    bit python

---
+++ nado_coding/dictionary.py
@@ -0,0 +1,39 @@
+cabinet = {3:"유재석", 100:"김태호"}
+print(cabinet[3])
+print(cabinet[100])
+
+print(cabinet.get(3))
+#print(cabinet[5]) # 오류를 발생시키고 프로그램 바로 종료
+print(cabinet.get(5)) # None으로 출력하고 뒤에 hi 까지 값을 출력
+print(cabinet.get(5, "사용가능")) # 값이 없을때 None이 아닌 default 값으로 사용가능 출력
+print("hi")
+
+print(3 in cabinet) # True
+print(5 in cabinet) # False
+
+cabinet = {"A-3":"유재석", "B-100":"김태호"}
+print(cabinet["A-3"])
+print(cabinet["B-100"])
+
+# 새 손님
+print(cabinet)
+cabinet["A-3"] = "김종국"
+cabinet["C-20"] = "조세호"
+print(cabinet)
+
+# 간 손님
+del cabinet["A-3"]
+print(cabinet)
+
+# key 들만 출력
+print(cabinet.keys())
+
+# value 들만 출력
+print(cabinet.values())
+
+# key, value 쌍으로 출력
+print(cabinet.items())
+
+# 목욕탕 폐점
+cabinet.clear()
+print(cabinet)
